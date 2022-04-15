b69797f1a8d5f20304a51b8af0fa4645d1a9da9a
Preston Darrough <techdarrough@gmail.com>
2022-04-14 20:04:17 -0700

    added about us and package with useState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
 
-import './App.css';
+import "./App.css";
 
-import Home from './components/Home'
-import About from './components/About'
-import Packages from './components/Packages'
-
+import Home from "./components/Home";
+import About from "./components/About";
+import Packages from "./components/Packages";
 
 function App() {
-  
-  const packages = ['Activate your Crystals', 'Monkey Meditation', 'Soak in the Hotsprings', 'Hypnotherapy', 'Mineral Bath']
-
+  const packages = [
+    "Activate your Crystals",
+    "Monkey Meditation",
+    "Soak in the Hotsprings",
+    "Hypnotherapy",
+    "Mineral Bath",
+  ];
 
   return (
     <div className="App">
@@ -18,36 +23,38 @@ function App() {
         <header>
           <h1 className="title">Welcome to Monty's Mineral SPA</h1>
 
-          <div className="navBar">
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About Us</Link>
-              </li>
-              <li>
-                <Link to="/packages">Our Packages</Link>
-              </li>
-            </ul>
-          </div>
+          <Container>
+            <Nav defaultActiveKey="/" variant="tabs" fill>
+              <Nav.Item>
+                <Nav.Link href="/">
+                  <Link to="/">Home</Link>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey={"aboutPage"}>
+                  <Link to="/about">About Us</Link>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey={"packagesPage"}>
+                  <Link to="/packages">Our Packages</Link>
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Container>
         </header>
 
         <div className="display">
           <Route path="/" component={Home} />
           <Route path="/about" component={About} />
-          <Route path="/packages" render={() => <Packages packages={packages}/>}  />
+          <Route
+            path="/packages"
+            render={() => <Packages packages={packages} />}
+          />
         </div>
-
       </Router>
-
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
--- src/components/Packages.js
+++ src/components/Packages.js
@@ -1,18 +1,28 @@
-
+import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container'
+import ListGroup from 'react-bootstrap/ListGroup'
+import { useState } from 'react'
 
 export default function Packages(props) {
-    const displayPackages = props.packages.map((eachPackage) => <li>{eachPackage}</li>)
-
+const [activeItem, setActiveItem] = useState(props.packages[0])
     return (
-        <div>
-            <div className="packages">
-                <div className="packagesHeader">
-                    <h1>Our Packages</h1>
-                </div>
-                <ul>
-                    {displayPackages}
-                </ul>
-            </div>
-        </div>
+        <Container>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Our Packages</Card.Title>
+                    <Card.Text>
+                        Check out some of our packages! Every package is ethically sourced and organic!
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup>
+                    {props.packages.map((eachPackage, i) => 
+        <ListGroup.Item key={i} active={eachPackage === activeItem} onClick={()=> setActiveItem(eachPackage)}>{eachPackage}</ListGroup.Item>)} 
+                </ListGroup>
+            </Card>
+        </Container>
     )
 }
+
+
+
+
